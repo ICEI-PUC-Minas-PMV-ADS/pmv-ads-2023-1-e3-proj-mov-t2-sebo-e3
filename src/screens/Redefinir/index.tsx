fc3d119b42d6fa6456b8d2b62223d984ca4202ed
RamonBicalho <magalhaesramon4@gmail.com>
2023-04-20 21:45:30 -0300

    Feat: Tela de Recuperar Senha

---
+++ src/screens/Redefinir/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import ButtonPrimary from "../../components/Forms/ButtonPrimary";
+import Input from "../../components/Forms/Input";
+import Label from "../../components/Forms/Label";
+import Spacer from "../../components/Spacer";
+import { ViewContainer } from "../../ui/style/style";
+
+import {
+  Form,
+  SubTitle
+} from "./style";
+
+function Redefinir({ navigation }) {
+  
+  function teste() {
+    console.log("redefinido");
+  }
+
+  return (
+    <ViewContainer>
+
+      <SubTitle>
+        Digite aqui o e-mail cadastrado, caso não possua ativar o cadastro:{"\n"}
+      </SubTitle>
+
+      <Form>
+        <Label title="E-mail" />
+        <Input placeholder="E-mail" />
+        <Spacer margin="xx" />
+
+        <SubTitle>
+        Digite a nova senha abaixo: {"\n"}
+      </SubTitle>
+
+        <Label title="Senha" />
+        <Input placeholder="Senha" />
+        <Spacer margin="xx" />
+
+        <Label title="Confirme a senha" />
+        <Input placeholder="Confirme a senha" />
+        <Spacer margin="xx" />
+
+        <SubTitle>
+        Sua senha deve conter: {"\n"} {"\n"} 
+        - Crie uma senha com no mínimo 8 caracteres {"\n"}
+        - Adicione letra minúsculas {"\n"}
+        - Adicione pelo menos uma letra maiúscula
+      </SubTitle>
+      <Spacer margin="xx" />
+
+
+        <ButtonPrimary title="Redefinir senha" onPress={teste} />
+      </Form>
+
+    </ViewContainer>
+  );
+}
+
+export default Redefinir;
