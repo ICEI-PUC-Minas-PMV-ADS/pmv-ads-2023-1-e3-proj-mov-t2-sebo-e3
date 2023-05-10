9ea9a8981cd4f686f19d2a378e0a689285c0b3a3
Herbert Nordson <herbert_nordson@hotmail.com>
2023-05-09 21:25:19 -0300

    alinhando capa

--- src/screens/ProductPage/index.tsx
+++ src/screens/ProductPage/index.tsx
@@ -4,53 +4,52 @@ import ButtonPrimary from "../../components/Forms/ButtonPrimary";
 import ButtonSecundary from "../../components/Forms/ButtonSecundary";
 import Spacer from "../../components/Spacer";
 import { Title, ViewContainer } from "../../ui/style/style";
-import { OrContainer, OrLine, SubTitle } from "./style"
+import { OrContainer, OrLine, SubTitle } from "./style";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
 
-
 function ProductPage({ navigation }) {
-    return (
-      <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
-
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
       <ViewContainer>
-        
-      <View style={{width: "100%"}}>
-        
-        <Image
-        source={require('../../assets/Capa.png')}
-        />
-      </View>
+        <View style={{alignItems: "center"}}>
+          <Image source={require("../../assets/Capa.png")}/>
+        </View>
 
         <Title>Trono de Vidro - Vol 1</Title>
 
-        <SubTitle>
-         Sarah J. Maas
-        </SubTitle>
+        <SubTitle>Sarah J. Maas</SubTitle>
         <Title>R$ 15,80</Title>
         <Spacer margin="xx" />
 
         <Title>Detalhes do produto</Title>
 
-      <OrContainer>
-        <OrLine></OrLine>
-        <OrLine></OrLine>
-      </OrContainer>
-  
+        <OrContainer>
+          <OrLine></OrLine>
+          <OrLine></OrLine>
+        </OrContainer>
+
         <SubTitle>
-         {"\n"}Autor: Sarah J. Maas {"\n"}Editora: Galera
-         {"\n"} Quantidade de páginas: 392 Páginas {"\n"}Estado de conservação: Bom
+          {"\n"}Autor: Sarah J. Maas {"\n"}Editora: Galera
+          {"\n"} Quantidade de páginas: 392 Páginas {"\n"}Estado de conservação:
+          Bom
         </SubTitle>
         <Spacer margin="xx" />
-        
-        <ButtonPrimary title="Adicionar ao carrinho" onPress={() => navigation.navigate('Login')} />
+
+        <ButtonPrimary
+          title="Adicionar ao carrinho"
+          onPress={() => navigation.navigate("Login")}
+        />
         <Spacer margin={"mx"} />
-  
-        <ButtonSecundary title="Comprar agora"  onPress={() => navigation.navigate('Cadastre-se')} />
+
+        <ButtonSecundary
+          title="Comprar agora"
+          onPress={() => navigation.navigate("Cadastre-se")}
+        />
         <Spacer margin={"mx"} />
       </ViewContainer>
-      </SafeAreaView>
-    );
-  }
-  
-  export default ProductPage;
+    </SafeAreaView>
+  );
+}
+
+export default ProductPage;
